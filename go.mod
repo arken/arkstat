7834e65be36a3183b3ec25b4dd8a0a1f73dedd8f
Alec Scott <alec@bcs.sh>
2021-07-31 11:57:10 -0700

    Use Chi for HTTP Router

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/arken/arkstat
 
 go 1.16
 
-require github.com/mattn/go-sqlite3 v1.14.8
+require (
+	github.com/go-chi/chi/v5 v5.0.3
+	github.com/mattn/go-sqlite3 v1.14.8
+)
--- web/web.go
+++ web/web.go
@@ -4,14 +4,19 @@ import (
 	"net/http"
 
 	"github.com/arken/arkstat/stats"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 var global *stats.Stats
 
 func Start(main *stats.Stats) {
 	global = main
+	// Setup Chi Router
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 	// Setup handler functions for api endpoints
-	http.HandleFunc("/usage", handleUsage)
+	r.Get("/usage", handleUsage)
 	// Start http server and listen for incoming connections
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", r)
 }
