60e2ae7c18d5ed93d2b93e4e32255691845db77d
Alec Scott <hi@alecbcs.com>
2021-12-06 15:00:43 -0700

    Set Content Type and Cache Headers

--- web/usage.go
+++ web/usage.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 )
 
 func handleUsage(w http.ResponseWriter, r *http.Request) {
@@ -12,5 +13,10 @@ func handleUsage(w http.ResponseWriter, r *http.Request) {
 		log.Printf("HTTP %d - %s", 500, err)
 		http.Error(w, "Server Error", 500)
 	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Cache-Control", "max-age:290304000, public")
+	w.Header().Set("Last-Modified", time.Now().Format(http.TimeFormat))
+	w.Header().Set("Expires", time.Now().Add(5*time.Minute).Format(http.TimeFormat))
 	w.Write(bytes)
 }
